b595c9d2f30a75f20de910726959bf111494138f
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-08-11 12:30:56 +0700

    Update sodosok.sh

--- shadowsocks/sodosok.sh
+++ shadowsocks/sodosok.sh
@@ -112,10 +112,10 @@ iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 cd /usr/bin
-wget -O addss "https://raw.githubusercontent.com/hokagelegend2023/alpha/main/shadowsocks/addss.sh"
-wget -O delss "https://raw.githubusercontent.com/hokagelegend2023/alpha/main/shadowsocks/delss.sh"
-wget -O cekss "https://raw.githubusercontent.com/hokagelegend2023/alpha/main/shadowsocks/cekss.sh"
-wget -O renewss "https://raw.githubusercontent.com/hokagelegend2023/alpha/main/shadowsocks/renewss.sh"
+wget -O addss "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/shadowsocks/addss.sh"
+wget -O delss "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/shadowsocks/delss.sh"
+wget -O cekss "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/shadowsocks/cekss.sh"
+wget -O renewss "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/shadowsocks/renewss.sh"
 chmod +x addss
 chmod +x delss
 chmod +x cekss
