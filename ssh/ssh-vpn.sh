f2b0422014e49672f6b6ae86e5bfa1012dd0551f
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-08-14 14:01:44 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -175,9 +175,9 @@ echo "/usr/sbin/nologin" >> /etc/shells
 
 # install squid (proxy nya aku matikan)
 cd
-apt -y install squid3
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/hokagelegend2023/alpha/main/ssh/squid3.conf"
-#sed -i $MYIP2 /etc/squid/squid.conf
+apt -y install squid
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/hokagelegend2023/alpha/main/ssh/squid.conf"
+sed -i $MYIP2 /etc/squid/squid.conf
 
 # Install SSLH
 apt -y install sslh
